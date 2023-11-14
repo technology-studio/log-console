4ef4ace8a532f73cfb21d2f29598e73d6bace250
Erik Slovak <erik.slovak@technologystudio.sk>
2023-11-14 18:09:05 +0100

    chore: migrate package

--- __tests__/Config/LogConfig.ts
+++ __tests__/Config/LogConfig.ts
@@ -8,20 +8,21 @@ import {
   configManager,
   Level,
 } from '@txo-peer-dep/log'
-import * as ConsoleLogger from '@txo/log-console/src'
+import * as ConsoleLogger from '@txo/log-console'
 // import * as ReactotronLogger from '@txo/log-reactotron'
 
 configManager.update({
   loggerConfigMap: {
     [ConsoleLogger.LOGGER_KEY]: {
       writeLog: ConsoleLogger.writeLog,
-      nodeEnvironmentList: ['production', 'development'],
+      nodeEnvironmentList: ['production', 'development', 'test'],
     },
     // [ReactotronLogger.LOGGER_KEY]: {
     //   writeLog: ReactotronLogger.writeLog,
     //   nodeEnvironmentList: ['development'],
     // },
   },
+  payloadProcessor: () => (payload: Record<string, unknown>) => JSON.stringify(payload, null, 2),
   defaultLevelForNodeEnvironmentMap: {
     production: Level.ERROR,
     development: Level.INFO,
